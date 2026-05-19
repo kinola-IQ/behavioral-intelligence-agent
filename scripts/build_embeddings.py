ac5401c3ba62d46bd7fd4300774ebf60643e39bb
akinola <akdunmininu@gmail.com>
2026-05-19 11:21:29 +0100

    updated the build embeddings scripts

--- scripts/build_embeddings.py
+++ scripts/build_embeddings.py
@@ -2,13 +2,14 @@
 import pandas as pd
 from src.embeddings.indexer import upload_data
 from src.logging.audit_log import log_event
+from src.config.constants import PROCESSED_DATA_DIR
 
 
 def main() -> None:
     """pipeline for indexing and uploading to vectordabase"""
+    data = PROCESSED_DATA_DIR / "persona_library_formatted_wide.csv"
     try:
-        data = pd.read_csv(
-            r'data\processed\persona_library_formatted_wide.csv')
+        data = pd.read_csv(data)
         upload_data(data)
         return log_event('vector database populated successfully')
     except Exception as exc:
