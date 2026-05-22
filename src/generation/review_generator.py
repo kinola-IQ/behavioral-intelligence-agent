7dbf37c05691677829d627409a36fc7a881c3d4a
akinola <akdunmininu@gmail.com>
2026-05-22 08:42:37 +0100

    refined comments

--- src/generation/review_generator.py
+++ src/generation/review_generator.py
@@ -13,11 +13,11 @@ from ..core.persona_builder import model_user
 from ..core.memory_layer import context_store
 from ..retrieval.search import retrieve_text
 
-
+# agent
 def review_generator_agent():
     """orchestrator in charge of deducing user persona and predicting review and rating"""
     return create_react_agent(
     utils.LLM,
-    tools = [model_user, context_store, retrieve_text],
+    tools = [model_user, context_stosre, retrieve_text],
     prompt= review_generation_prompt()
     )
