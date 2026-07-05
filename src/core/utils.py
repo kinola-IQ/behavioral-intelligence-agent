915a12215410bb32c313ca2c14a2133fbb2e9b3a
akinola <akdunmininu@gmail.com>
2026-07-05 19:58:56 +0100

    tweaked startup

--- src/core/utils.py
+++ src/core/utils.py
@@ -46,12 +46,16 @@ async def startup_resources() -> None:
     LLM = ChatGroq(
         # model="meta-llama/llama-4-scout-17b-16e-instruct",
         model="llama-3.1-8b-instant",
-        groq_api_key=os.environ['GROQ_API_KEY']
+        # groq_api_key=os.environ['GROQ_API_KEY']
+        groq_api_key=os.environ.get('GROQ_API_KEY', None)
+
         )
 
     # model provider to be used for chat
     HF_LLM_PROVIDER = InferenceClient(
-        api_key=os.environ["HUGGINGFACE_API_KEY"]
+        # api_key=os.environ["HUGGINGFACE_API_KEY"]
+        api_key=os.environ.get("HUGGINGFACE_API_KEY", None)
+
     )
 
     log_event(
